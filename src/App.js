8f583795181f39e8961468be2a0ac577f4c580aa
CAMA5163 <caroline.malmberg@gmail.com>
2024-03-25 12:12:48 +0100

    changed app text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload. Code edited again! 
-          And again to test it!
+          And again to test it! Just keep on editing and testing...
         </p>
         <a
           className="App-link"
